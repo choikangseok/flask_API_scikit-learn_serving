c42ddb87bd0e2db3f9155b2a87de248f4cbe0c31
choikangseok <ktos5427@naver.com>
2022-03-24 18:04:36 +0900

    Modify ModelServing

--- ModelServingApp.py
+++ ModelServingApp.py
@@ -1,43 +1,44 @@
 # flask_server.py
-
+import tensorflow.keras as keras
 import numpy as np
-import config
-import logging
-import tensorflow as tf
-import rf_feature_extract
-import cnn_feature_extract
+from config import *
+from rf_feature_extract import *
+from cnn_feature_extract import *
+from google_repuCheck import *
 from flask import Flask, request
+from keras.models import load_model
 import pickle
 
 
-RF_MODEL = "model/RF.pkl"
-CNN_MODEL = "model/CNN.h5"
+#random forest model
+with open(RF_MODEL, 'rb') as rf_file:
+    rf_model = pickle.load(rf_file)
 
-
-with open(RF_MODEL, 'rb') as file:
-    rf_model = pickle.load(file)
-
-with open(CNN_MODEL, 'rb') as file:
-    cnn_model = pickle.load(file)
+#cnn model
+with open(CNN_MODEL, 'rb') as cnn_file:
+    cnn_model = load_model.load(cnn_file)
 
 
 app = Flask(__name__)
+
 @app.route('/rfcnn', methods=['POST'])
-def inference():
+def rfcnn():
     result = {}
 
     data = request.get_json()
-    rf_feature = np.array(rf_feature_extract(data['URLs'])) # Feature_extract() return list
+    rf_feature = np.array(rf_feature_extract(data['URL'])) # Feature_extract() return list
     # rf_feature : [1,2,3,4,5,1 ...]
-    cnn_feature = np.array(cnn_feature_extract(data['URLs'])) # Feature_extract() return list
+    cnn_feature = np.array(cnn_feature_extract(data['URL'])) # Feature_extract() return list
     # cnn_feature : [[[255, 255, 255], [255, 255, 255], [255, 255,...
     result['RFmodel'] = {'predict' : int(rf_model.predict([rf_feature])*100)}
     result['CNNmodel'] = {'predict' : int(cnn_model.predict([cnn_feature])*100)}
+    result['SafeBrowsing'] = {'malicious' : repuCheck(data['URL'])}
 
 
-    logging.debug(f"[result] {result}")
+
+
 
     return result, 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8888, threaded=False)
+    app.run(host='0.0.0.0', port=8888, threaded=True, debug=True)
---
+++ test.py
@@ -0,0 +1,6 @@
+import rf_feature_extract
+
+
+result = rf_feature_extract("http://naver.com")
+
+print(result)
